a74347dd8a8b5c5891e442325225def79220c271
Alessandro Ronchi <aleron75@gmail.com>
2020-03-27 09:20:56 +0100

    Update bio and env files

--- bin/start_jekyll.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-alias jekyll='sudo docker run --rm -v "$PWD:/src" -p 4000:4000 grahamc/jekyll'
-sudo docker run --name jekyll -d -v "$PWD:/src" -p 4000:4000 grahamc/jekyll serve -I -H 0.0.0.0
